a4023b1d7449121ddc388ca3171247bb126fc5dc
Jeff mefkur <156502715+XUNILATSU@users.noreply.github.com>
2024-01-13 03:37:36 -0500

    opensUrls
    #differences in code between python version,*

---
+++ OpenURLS.py
@@ -0,0 +1,15 @@
+import urllib
+import webbrowser
+import time
+webbrowser.open('http://www.google.com')
+#This allows default browser to load, so new tabs will not be windowed.
+time.sleep(2.1)
+webbrowser.open_new_tab('http://www.whatsmyip.org/')
+webbrowser.open_new_tab('https://www.facebook.com')
+webbrowser.open_new_tab('https://app.mysms.com/')
+webbrowser.open_new_tab('http://codeacademy.com')
+webbrowser.open_new_tab('http://www.tutorialspoint.com/python/python_strings.htm')
+webbrowser.open_new_tab('https://docs.python.org/2/library/webbrowser.html')
+webbrowser.open_new_tab('https://www.titips.com')
+webbrowser.open_new_tab('https://www.tiktok.com')
+
