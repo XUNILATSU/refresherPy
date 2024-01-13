4ca3bd56dd3ae9869e26a5d28e01d5cece2e6cf9
Jeff mefkur <156502715+XUNILATSU@users.noreply.github.com>
2024-01-13 03:43:41 -0500

    writesSecretMessagetoAfile
    hexdump fule you want to write too

---
+++ main.py
@@ -0,0 +1,5 @@
+end_hex = b"\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60\x82"
+
+with open('image.png', 'ab') as f:
+	f.write(b"Well, my name is jeff and im hurting pretty bad, i mean im fighting weapons of mass destruction to keep my head afloat, this is one big ass conspiracy all for bill gates's power")
+		 
